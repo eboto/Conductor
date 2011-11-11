13a2d56c52c3d58b845292855ef37348799c3bf9
Andrew Smith <drewsmits@gmail.com>
2011-11-11 14:20:53 -0800

    Added MIT open source license

--- Conductor/Conductor.h
+++ Conductor/Conductor.h
@@ -3,7 +3,24 @@
 //  Conductor
 //
 //  Created by Andrew Smith on 10/21/11.
-//  Copyright (c) 2011 Andrew B. Smith. All rights reserved.
+//  Copyright (c) 2011 Andrew B. Smith ( http://github.com/drewsmits ). All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal 
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+// of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included 
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import <Foundation/Foundation.h>
@@ -17,23 +34,25 @@
 @property (nonatomic, readonly) NSMutableDictionary *queues;
 
 /**
- * Singleton Conductor instance
+ Singleton Conductor instance
  */
 + (id)sharedInstance;
 
 /**
- * Creates a queue based on the operations class name and adds the operation to
- * it.  Will not create a new operation queue if one by that name already exists.
- * Returns the operation identifier.
- * @see queueNameForOperation:
+ Creates a queue based on the operations class name and adds the operation to
+ it.  Will not create a new operation queue if one by that name already exists.
+ Returns the operation identifier.
+ 
+ @see queueNameForOperation:
  */
 - (void)addOperation:(CDOperation *)operation;
 
 /**
- * Creates a queue based on the operations class name and adds the operation to
- * it at the given priority.  Will not create a new operation queue if the one
- * by that name already exists.  Returns the operation identifer.
- * @see queueNameForOperation:
+ Creates a queue based on the operations class name and adds the operation to
+ it at the given priority.  Will not create a new operation queue if the one
+ by that name already exists.  Returns the operation identifer.
+ 
+ @see queueNameForOperation:
  */
 - (void)addOperation:(CDOperation *)operation 
           atPriority:(NSOperationQueuePriority)priority;
